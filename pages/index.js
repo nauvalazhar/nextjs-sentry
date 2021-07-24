4c9f87e608db9a23de1c9de750474e9543b5a2fb
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2021-07-24 15:47:58 +0700

    koala (2)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import { captureException, setContext } from "@sentry/nextjs";
+
+export async function getServerSideProps() {
+  try {
+    const a = await fetch('https://github.com/aksdjlaksjdklasd', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ajsdlkajslkdajsd alsdasd'
+      }
+    });
+    const r = await a.json();
+  } catch (e) {
+    setContext("getUserInfo", {
+      id: 12,
+      token: 'asdasdajfl09123'
+    });
+    captureException(e);
+  }
+
+  return {
+    props: {}
+  }
+}
+
 export default function Home() {
   return (
     <div>
